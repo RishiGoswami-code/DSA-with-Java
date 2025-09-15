c97839195b4e34c89df41ace0361f9ce7bc2b353
GseriesCom <chandangi2005@hmail.com>
2025-09-15 22:18:29 +0530

    Base conversion problem added

---
+++ Maths for DSA/Anybase.java
@@ -0,0 +1,9 @@
+//We are here to make a class that will take care of some important conversion of a number from one base to another
+//If a person use this class, its method will take care of it 
+
+
+public class Anybase{
+  public void decToBin(int num){
+    
+  }
+}
